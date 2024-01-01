dc015557f92d4848fae545ef6f9b942303cbc39d
Ankit <ankitjsr9@gmail.com>
2024-01-01 12:07:25 +0530

    Shifted to new server

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const mysql = require("mysql");
 const path = require("path");
+const fs = require("fs");
 
 var databaseConnectionSuccess = false;
+
 const connection = mysql.createConnection({
-  host: "sql12.freemysqlhosting.net",
-  user: "sql12673332",
-  password: "t5kLGfHsm3",
-  database: "sql12673332",
+  host: "mysql-147f299b-netflixpr45-2823.a.aivencloud.com",
+  user: "ankit",
+  password: "AVNS_7Xj0XqGo9KoTM5a9SMn",
+  database: "country_state_city",
+  port: 25447,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(__dirname + "/ssl/ca.pem").toString(),
+  },
 });
 try {
   try {
@@ -44,6 +51,8 @@ const getCountryList = (request, response) => {
           status: false,
           error: "Something went wrong. Please try again later.",
           data: [],
+          a: error,
+          b: results,
         });
       } else {
         return response.status(200).json({
